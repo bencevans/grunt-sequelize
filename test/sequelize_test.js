d140cdd774b3a7d3155be8037ead58598c499b18
Ben Evans <ben@bensbit.co.uk>
2013-11-21 15:46:05 +0000

    Updates ready for tests

--- test/sequelize_test.js
+++ test/sequelize_test.js
@@ -4,11 +4,26 @@ var assert = require('assert');
 var grunt  = require('grunt');
 var exec   = require('child_process').exec
 
+
+grunt.task.init = function() {};
+
+// Init config
+grunt.initConfig({
+  sequelize: {
+    options: {
+      dialect: 'sqlite',
+      storage: 'path/to/database.sqlite'
+    }
+  }
+});
+
+
 describe('grunt-sequelize', function() {
 
   describe('sequelize:migrate', function() {
     describe('without any arguments', function() {
       it('should migrate to the top migration');
+      // grunt.util.spawn({ grunt: true, args: ['sequelize:migrate'] }, function() {}) 
     });
     describe('with a migration argument', function() {
 
@@ -17,6 +32,7 @@ describe('grunt-sequelize', function() {
 
   describe('sequelize:undo', function() {
     it('should undo the last migration');
+    // grunt.util.spawn({ grunt: true, args: ['sequelize:undo'] }, function() {}) 
   });
 
 });
