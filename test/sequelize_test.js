2a52d110a3311635586f12de54f684d8589db4fa
Ben Evans <ben@bensbit.co.uk>
2013-11-27 13:30:19 +0000

    Setup test definitions for new features

--- test/sequelize_test.js
+++ test/sequelize_test.js
@@ -33,18 +33,33 @@ describe('grunt-sequelize', function() {
       });
     });
     describe('with a migration argument', function() {
+      describe('where the migration is the same as the current state of the database', function() {
+        it('should just inform the user');
+      });
+      describe('where the migration is higher than the current state of the database', function(){
+        it('should migrate to the specified migration');
+      });
+      describe('where the migration is lower than the current state of the database', function(){
+        it('should migrate to the specified migration');
+      });
     });
   });
 
 
   describe('sequelize:undo', function() {
-    it('should undo the last migration', function(done) {
-      grunt.util.spawn({ grunt: true, args: ['sequelize:undo'] }, function(error, result) {
-        console.log(result);
-        assert.equal(result.code, 0);
-        done();
+    describe('without any arguments', function() {
+      it('should undo the last migration', function(done) {
+        grunt.util.spawn({ grunt: true, args: ['sequelize:undo'] }, function(error, result) {
+          console.log(result);
+          assert.equal(result.code, 0);
+          // TODO assert migration ID === last ID
+          done();
+        });
       });
     });
+    describe('with a set number to undo', function() {
+      it('should migrate downwards by the given number of migrations');
+    })
   });
 
 });
